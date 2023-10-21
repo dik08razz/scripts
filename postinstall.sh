84ee450807cebef563ad9ee3d79393b324ad64ff
Yan Tkach <tkach.yan@gmail.com>
2023-10-21 16:17:40 +0300

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -9,6 +9,7 @@ echo ""
 
     sudo apt update -y
     sudo apt upgrade -y
+    clear
 
 echo ""
 echo "Step 2: Kubuntu Backports"
@@ -17,6 +18,7 @@ echo ""
     sudo add-apt-repository ppa:kubuntu-ppa/backports -y
     sudo apt update -y
     sudo apt upgrade -y
+    clear
 
 echo ""
 echo "Step 3: XanMod"
@@ -26,6 +28,7 @@ echo ""
     echo 'deb [signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-release.list
     sudo apt update -y
     sudo apt install linux-xanmod-x64v2 -y
+    clear
 
 echo ""
 echo "Step 4: Apps"
@@ -33,6 +36,7 @@ echo ""
     wget -P $HOME/Downloads https://cdn.cloudflare.steamstatic.com/client/installer/steam.deb
 
     sudo apt install mc neofetch flatpak htop gnome-disk-utility qbittorrent mangohud goverlay gamemode timeshift ssh samba ./Downloads/*.deb -y
+    clear
 
 echo ""
 echo "Step 5: Setup Flathub"
@@ -40,7 +44,8 @@ echo ""
 
     flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
     flatpak install telegram flatseal pupgui discord heroic lutris bottles -y
-    
+    clear
+
 echo ""
 echo "Step 6: Remove apps"
 echo ""
@@ -50,9 +55,5 @@ echo ""
     sudo apt autoremove -y
     sudo rm -rf $HOME/Downloads/*
 echo ""
-
     clear
-    neofetch
-
-echo ""
-echo "Finish =)"
+    reboot
