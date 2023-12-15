6308fd03953421cd2149b80e241dd8aa42373041
dik08razz <tkach.yan@gmail.com>
2023-12-15 18:37:46 +0200

    151223

--- fedoraKDE.sh
+++ fedoraKDE.sh
@@ -32,7 +32,7 @@ echo ""
 sleep 2
 
     sudo dnf upgrade --refresh
-    sudo dnf mc neofetch htop gnome-disk-utility mangohud goverlay zsh steam google-chrome-stable -y
+    sudo dnf mc neofetch htop mangohud goverlay zsh steam google-chrome-stable -y
     flatpak install telegram flatseal pupgui discord heroic lutris -y
     clear
 
