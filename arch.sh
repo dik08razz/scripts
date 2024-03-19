d8fe4e63ee59813ed6149290e1c658da474ad8ed
Yan Tkach <tkach.yan@gmail.com>
2024-03-20 00:42:50 +0200

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -3,16 +3,16 @@
 #var
 baseapps="mc htop neofetch timeshift"
 kdeapps="filelight ark dolphin dolphin-plugins elisa gwenview kate kcalc kcron kdeconnect kdenetwork-filesharing kdenlive kdialog kfind kgpg kjournald krdc krfb ksystemlog kwalletmanager okular partitionmanager signon-kwallet-extension spectacle" 
-asianfonts="adobe-source-han-sans-cn-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts adobe-source-han-sans-otc-fonts adobe-source-han-sans-tw-fonts adobe-source-han-serif-cn-fonts adobe-source-han-serif-jp-fonts adobe-source-han-serif-kr-fonts adobe-source-han-serif-otc-fonts adobe-source-han-serif-tw-fonts"
-fonts="cantarell-fonts freetype noto-fonts noto-fonts-emoji noto-fonts-cjk noto-fonts-extra ttf-bitstream-vera ttf-dejavu ttf-liberation ttf-opensans"
+fonts="cantarell-fonts freetype noto-fonts noto-fonts-emoji noto-fonts-cjk noto-fonts-extra ttf-bitstream-vera ttf-dejavu ttf-liberation ttf-opensans adobe-source-han-sans-cn-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts adobe-source-han-sans-otc-fonts adobe-source-han-sans-tw-fonts adobe-source-han-serif-cn-fonts adobe-source-han-serif-jp-fonts adobe-source-han-serif-kr-fonts adobe-source-han-serif-otc-fonts adobe-source-han-serif-tw-fonts"
 power="power-profiles-daemon upower"
 virtualization="virt-manager qemu-full"
+networks="openvpn networkmanager-openvpn"
 gaming="steam mangohud goverlay"
 social="telegram-desktop discord"
 
 cd $HOME
 
-sudo pacman -S base-devel $gaming $social $baseapps $kdeapps $power $virtualization
+sudo pacman -S base-devel $gaming $social $baseapps $kdeapps $power $virtualization $networks
 
 git clone https://aur.archlinux.org/yay-bin.git
 cd $HOME/yay-bin
